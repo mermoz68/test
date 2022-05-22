fdfc4722ecab33c640ae4bd4829ded0480661bc2
mermoz68 <47685555+mermoz68@users.noreply.github.com>
2022-05-22 07:13:34 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
 basic.forever(function () {
-	
+    motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 50)
+    basic.pause(1000)
+    motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, 50)
+    basic.pause(1000)
 })
